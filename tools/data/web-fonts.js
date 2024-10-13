769526c30ffb5e9c6aab57a96039a83ab67abd93
webdevbynight <git@victor-brito.dev>
2024-10-13 03:58:40 +0200

    Initial commit

---
+++ tools/data/web-fonts.js
@@ -0,0 +1,12 @@
+/**
+ *  The fonts and weights to install must be declared as follows:
+ * {
+ *      "Name_of_the_font": ["Bold", "Italic", "Regular"],
+ *      "Name_of_the_variable_font": ["Italic-VariableFont_wght", "VariableFont_wght"]
+ * }
+ */
+const webFontsToInstall = {
+    Rubik: ["VariableFont_wght"]
+};
+
+export default webFontsToInstall;
